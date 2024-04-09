4edeb52d929381ce398b21d4f22feffe72f5c715
Harshit P G <harshitpg2004@gmail.com>
2024-04-09 11:29:01 +0530

    minor update

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -11,7 +11,6 @@ const UserSchema = new Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
       trim: true,
       lowercase: true,
       match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
