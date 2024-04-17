34b2ec65c9e8a782c69e28eacf0eb06ad1878f61
Harshit P G <harshitpg2004@gmail.com>
2024-04-17 17:46:39 +0530

    modified Application Status

--- api/controllers/statusController.js
+++ api/controllers/statusController.js
@@ -38,6 +38,223 @@ const applicationStatus = async (req, res) => {
   }
 };
 
+const applicationTechStatus = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await UserModel.findOne({
+      user_id: id,
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    if (!user.domain.includes("tech")) {
+      return res.status(200).json({
+        message: "You have not applied for the Tech Domain.",
+      });
+    }
+
+    if (!user.isTechDone && !user.roundOne) {
+      return res.status(200).json({
+        message:
+          "Your application for the Tech Domain is not submitted. Kindly complete and submit it for evaluation.",
+      });
+    }
+
+    if (user.isTechDone && !user.roundOne) {
+      return res.status(200).json({
+        message:
+          "Your application for the Tech Domain is under evaluation. Stay tuned for results.",
+      });
+    }
+
+    if (user.isTechDone && user.roundOne && user.roundTwo) {
+      return res.status(200).json({
+        message:
+          "Congratulations! You have been selected for the second round of Tech. All the best.",
+      });
+    }
+
+    if (user.isTechDone && user.roundOne && user.roundTwo && user.roundThree) {
+      return res.status(200).json({
+        message:
+          "Congratulations! You have been selected for the third round. All the best.",
+      });
+    }
+
+    if (
+      user.isTechDone &&
+      user.roundOne &&
+      user.roundTwo &&
+      user.roundThree &&
+      user.isCore
+    ) {
+      return res.status(200).json({
+        message: "Congratulations! You have been selected.",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Unknown application status.",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const applicationDesignStatus = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await UserModel.findOne({
+      user_id: id,
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    if (!user.domain.includes("design")) {
+      return res.status(200).json({
+        message: "You have not applied for the design Domain.",
+      });
+    }
+
+    if (!user.isDesignDone && !user.roundOne) {
+      return res.status(200).json({
+        message:
+          "Your application for the Tech Domain is not submitted. Kindly complete and submit it for evaluation.",
+      });
+    }
+
+    if (user.isDesignDone && !user.roundOne) {
+      return res.status(200).json({
+        message:
+          "Your application for the Tech Domain is under evaluation. Stay tuned for results.",
+      });
+    }
+
+    if (user.isDesignDone && user.roundOne && user.roundTwo) {
+      return res.status(200).json({
+        message:
+          "Congratulations! You have been selected for the second round of Tech. All the best.",
+      });
+    }
+
+    if (
+      user.isDesignDone &&
+      user.roundOne &&
+      user.roundTwo &&
+      user.roundThree
+    ) {
+      return res.status(200).json({
+        message:
+          "Congratulations! You have been selected for the third round. All the best.",
+      });
+    }
+
+    if (
+      user.isDesignDone &&
+      user.roundOne &&
+      user.roundTwo &&
+      user.roundThree &&
+      user.isCore
+    ) {
+      return res.status(200).json({
+        message: "Congratulations! You have been selected.",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Unknown application status.",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const applicationManagementStatus = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await UserModel.findOne({
+      user_id: id,
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    if (!user.domain.includes("management")) {
+      return res.status(200).json({
+        message: "You have not applied for the management Domain.",
+      });
+    }
+
+    if (!user.isManagementDone && !user.roundOne) {
+      return res.status(200).json({
+        message:
+          "Your application for the management Domain is not submitted. Kindly complete and submit it for evaluation.",
+      });
+    }
+
+    if (user.isManagementDone && !user.roundOne) {
+      return res.status(200).json({
+        message:
+          "Your application for the management Domain is under evaluation. Stay tuned for results.",
+      });
+    }
+
+    if (user.isManagementDone && user.roundOne && user.roundTwo) {
+      return res.status(200).json({
+        message:
+          "Congratulations! You have been selected for the second round of Tech. All the best.",
+      });
+    }
+
+    if (
+      user.isManagementDone &&
+      user.roundOne &&
+      user.roundTwo &&
+      user.roundThree
+    ) {
+      return res.status(200).json({
+        message:
+          "Congratulations! You have been selected for the third round. All the best.",
+      });
+    }
+
+    if (
+      user.isManagementDone &&
+      user.roundOne &&
+      user.roundTwo &&
+      user.roundThree &&
+      user.isCore
+    ) {
+      return res.status(200).json({
+        message: "Congratulations! You have been selected.",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Unknown application status.",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 module.exports = {
+  applicationTechStatus,
+  applicationDesignStatus,
+  applicationManagementStatus,
   applicationStatus,
 };
--- api/routes/statusRoute.js
+++ api/routes/statusRoute.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { applicationStatus } = require("../controllers/statusController");
+const {
+  applicationStatus,
+  applicationTechStatus,
+  applicationDesignStatus,
+  applicationManagementStatus,
+} = require("../controllers/statusController");
 const validateToken = require("../middleware/validateTokenHandler");
 const validateVerify = require("../middleware/validateVerify");
 const app = express();
@@ -9,5 +14,23 @@ app.use(express.json());
 const router = express.Router();
 
 router.get("/status/:id", validateToken, validateVerify, applicationStatus);
+router.get(
+  "/statustech/:id",
+  validateToken,
+  validateVerify,
+  applicationTechStatus
+);
+router.get(
+  "/statusdesign/:id",
+  validateToken,
+  validateVerify,
+  applicationDesignStatus
+);
+router.get(
+  "/statusmanagement/:id",
+  validateToken,
+  validateVerify,
+  applicationManagementStatus
+);
 
 module.exports = router;
