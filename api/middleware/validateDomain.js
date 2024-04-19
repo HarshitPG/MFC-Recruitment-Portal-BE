53766c201230f4002a3b6e19d4de463cc20426ab
Harshit P G <harshitpg2004@gmail.com>
2024-04-20 00:45:51 +0530

    Update validateDomain.js

--- api/middleware/validateDomain.js
+++ api/middleware/validateDomain.js
@@ -1,35 +1,42 @@
 const jwt = require("jsonwebtoken");
-// const userModel = require("../models/userModel");
+const userModel = require("../models/userModel");
+
+const User = require("../models/User"); // Import the User model
 
 const validateTech = async (req, res, next) => {
+  let token;
+  let authHeader = req.headers.authorization || req.headers.Authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res
+      .status(401)
+      .json({ message: "User is not authorized or token missing" });
+  }
+
+  token = authHeader.split(" ")[1];
   try {
-    let token;
-    let authHeader = req.headers.authorization || req.headers.Authorization;
-
-    if (!authHeader || !authHeader.startsWith("Bearer")) {
-      return res
-        .status(401)
-        .json({ message: "User is not authorized or token missing" });
-    }
-
-    token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECERT);
-    const user = decoded;
-    console.log("decoded", decoded);
+    const userId = decoded.id;
+    const user = await User.findById(userId);
+    console.log("user", user);
+
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    console.log("user", user);
+
     const userSubdomain = user.domain;
     const userVerified = user.verified;
 
-    if (userVerified && userSubdomain.includes("tech")) {
+    if (
+      userVerified === true &&
+      userSubdomain &&
+      userSubdomain.includes("tech")
+    ) {
       return next();
-    } else {
-      return res
-        .status(403)
-        .json({ message: "User not verified for this route or choose domain" });
     }
+    return res
+      .status(403)
+      .json({ message: "User not verified for this route or choose domain" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -49,22 +56,27 @@ const validateDesign = async (req, res, next) => {
   token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECERT);
-    const user = decoded;
-    console.log("decoded", decoded);
+    const userId = decoded.id;
+    const user = await User.findById(userId);
+    console.log("user", user);
+
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    console.log("user", user);
-    const userVerified = user.verified;
-    const userSubdomain = user.domain;
 
-    if (userVerified && userSubdomain.includes("design")) {
+    const userSubdomain = user.domain;
+    const userVerified = user.verified;
+
+    if (
+      userVerified === true &&
+      userSubdomain &&
+      userSubdomain.includes("design")
+    ) {
       return next();
-    } else {
-      return res
-        .status(403)
-        .json({ message: "User not verified for this route or choose domain" });
     }
+    return res
+      .status(403)
+      .json({ message: "User not verified for this route or choose domain" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -72,34 +84,38 @@ const validateDesign = async (req, res, next) => {
 };
 
 const validateManagement = async (req, res, next) => {
+  let token;
+  let authHeader = req.headers.authorization || req.headers.Authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res
+      .status(401)
+      .json({ message: "User is not authorized or token missing" });
+  }
+
+  token = authHeader.split(" ")[1];
   try {
-    let token;
-    let authHeader = req.headers.authorization || req.headers.Authorization;
-
-    if (!authHeader || !authHeader.startsWith("Bearer")) {
-      return res
-        .status(401)
-        .json({ message: "User is not authorized or token missing" });
-    }
-
-    token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECERT);
-    const user = decoded;
-    console.log("decoded", decoded);
+    const userId = decoded.id;
+    const user = await User.findById(userId);
+    console.log("user", user);
+
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    console.log("user", user);
+
     const userSubdomain = user.domain;
     const userVerified = user.verified;
-
-    if (userVerified && userSubdomain.includes("management")) {
+    if (
+      userVerified === true &&
+      userSubdomain &&
+      userSubdomain.includes("management")
+    ) {
       return next();
-    } else {
-      return res
-        .status(403)
-        .json({ message: "User not verified for this route or choose domain" });
     }
+    return res
+      .status(403)
+      .json({ message: "User not verified for this route or choose domain" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
