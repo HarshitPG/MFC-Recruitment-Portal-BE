0e1b650d852fafb302ec3cfe05c296109d108f2d
Harshit P G <harshitpg2004@gmail.com>
2024-05-19 14:53:45 +0530

    .

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -177,7 +177,7 @@ const resendOTP = async (req, res) => {
         email: email,
       });
       if (!user.verified) {
-        await VerificationModel.deleteMany({ id });
+        await VerificationModel.deleteMany({ user_id });
         await sendVerificationMail(user);
         res.status(200).json({ message: "sent otp again" });
       } else {
